49393045a4e40e0b74c0902e0e4e507c1cd89af3
Omar Flores Grimontt <omar.flores@rappi.com>
2021-05-13 22:13:35 -0500

    Added app

---
+++ src/App.js
@@ -0,0 +1,4 @@
+import withFeedback from "domains/feedback";
+import { Home } from "pages";
+
+export default withFeedback(Home)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import { Home } from "./pages";
-import withFeedback from "domains/feedback";
+import App from './App'
 import "./index.css";
-
-const DecoratedComponent = withFeedback(Home)
 
 render(
   <React.StrictMode>
-    <DecoratedComponent/>
+    <App/>
   </React.StrictMode>,
   document.getElementById("root")
 );
